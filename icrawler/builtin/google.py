08a92d34cd653ed4a37b858d66a292986976fe40
Julian Freyberg <62940341+jfreyberg@users.noreply.github.com>
2022-12-19 20:35:57 +0100

    Update google.py

--- icrawler/builtin/google.py
+++ icrawler/builtin/google.py
@@ -141,14 +141,14 @@ class GoogleFeeder(Feeder):
 
 class GoogleParser(Parser):
     def parse(self, response):
-    soup = BeautifulSoup(
-        response.content.decode('utf-8', 'ignore'), 'lxml')
-    images = soup.find_all(name='img')
-    uris = []
-    for img in images:
-        if img.has_attr('src'):
-            uris.append(img['src'])
-    return [{'file_url': uri} for uri in uris]
+        soup = BeautifulSoup(
+            response.content.decode('utf-8', 'ignore'), 'lxml')
+        images = soup.find_all(name='img')
+        uris = []
+        for img in images:
+            if img.has_attr('src'):
+                uris.append(img['src'])
+        return [{'file_url': uri} for uri in uris]
     
     def parse(self, response):
         soup = BeautifulSoup(
